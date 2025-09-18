1ee9ca44dae381ae410d619f7434eecd57441508
sureshnenavath <suryanss143@gmail.com>
2025-09-18 20:07:05 +0530

    Add SPA redirects for Netlify and make CORS origins configurable via env

--- backend/subscribely/settings.py
+++ backend/subscribely/settings.py
@@ -147,12 +147,7 @@ SIMPLE_JWT = {
 }
 
 # CORS settings
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173,http://127.0.0.1:5173', cast=lambda v: [s.strip() for s in v.split(',')])
 
 CORS_ALLOW_CREDENTIALS = True
 
